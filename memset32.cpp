0d5445b064425129c5a40ea9a03dbaa782b2170c
Gabriel Ravier <gabravier@gmail.com>
2019-10-28 12:40:45 +0100

    Moved function array declarations inside mains
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- memset32.cpp
+++ memset32.cpp
@@ -19,6 +19,8 @@ extern "C"
 	mkMemsetDecl(asmlibSSE2Memset)
 	mkMemsetDecl(asmlibSSE2v2Memset)
 	mkMemsetDecl(asmlibAVXMemset)
+	mkMemsetDecl(asmlibAVX512FMemset)
+	mkMemsetDecl(asmlibAVX512BWMemset)
 	mkMemsetDecl(msvc2003Memset)
 	mkMemsetDecl(bytewiseMemset)
 	mkMemsetDecl(minixMemset)
@@ -30,41 +32,58 @@ extern "C"
 	mkMemsetDecl(inlineStringOpGccNoconaMemset)
 }
 
-const static std::array<memsetFunc, 27> funcs =
-{
-    {
-		{memset, "system libc memset"},
-		{pdclibMemset, "pdclib memset"},
-		{cloudlibcMemset, "cloudlibc memset"},
-		{klibcMemset, "klibc memset"},
-		{neatlibcMemset, "neatlibc memset"},
-		{dietlibcMemset, "dietlibc memset"},
-		{uClibcMemset, "uClibc memset"},
-		{newlibMemset, "newlib memset"},
-		{newlibSmallMemset, "newlib memset optimized for size"},
-		{muslMemset, "musl memset"},
-		{bionicSSE2AtomMemset, "bionic SSE2 Atom memset"},
-		{glibcMemset, "glibc memset"},
-		{glibcI586Memset, "glibc i586 memset"},
-		{glibcI686Memset, "glibc i686 memset"},
-		{asmlibMemset, "asmlib memset"},
-		{asmlibSSE2Memset, "asmlib SSE2 memset"},
-		{asmlibSSE2v2Memset, "asmlib SSE2 memset v2"},
-		{asmlibAVXMemset, "asmlib AVX memset"},
-		{msvc2003Memset, "MSVC 2003 memset"},
-		{bytewiseMemset, "bytewise memset"},
-		{minixMemset, "minix memset"},
-		{freeBsdMemset, "FreeBSD memset"},
-		{inlineStringOpGccMemset, "-minline-all-stringops gcc memset"},
-		{inlineStringOpGccI386Memset, "-minline-all-stringops -march=i386 gcc memset"},
-		{inlineStringOpGccI486Memset, "-minline-all-stringops -march=i486 gcc memset"},
-		{inlineStringOpGccI686Memset, "-minline-all-stringops -march=i686 gcc memset"},
-		{inlineStringOpGccNoconaMemset, "-minline-all-stringops -march=nocona gcc memset"}
-    }
-};
-
 int main()
 {
+	const static std::array<memsetFunc, 29> funcs =
+	{
+		{
+			{memset, "system libc memset"},
+			{pdclibMemset, "pdclib memset"},
+			{cloudlibcMemset, "cloudlibc memset"},
+			{klibcMemset, "klibc memset"},
+			{neatlibcMemset, "neatlibc memset"},
+			{dietlibcMemset, "dietlibc memset"},
+			{uClibcMemset, "uClibc memset"},
+			{newlibMemset, "newlib memset"},
+			{newlibSmallMemset, "newlib memset optimized for size"},
+			{muslMemset, "musl memset"},
+
+	#ifdef HAVE_SSE2
+			{bionicSSE2AtomMemset, "bionic SSE2 Atom memset"},
+	#endif
+
+			{glibcMemset, "glibc memset"},
+			{glibcI586Memset, "glibc i586 memset"},
+			{glibcI686Memset, "glibc i686 memset"},
+			{asmlibMemset, "asmlib memset"},
+	#ifdef HAVE_SSE2
+			{asmlibSSE2Memset, "asmlib SSE2 memset"},
+			{asmlibSSE2v2Memset, "asmlib SSE2 memset v2"},
+	#endif
+
+	#ifdef HAVE_AVX
+			{asmlibAVXMemset, "asmlib AVX memset"},
+	#endif
+
+	#ifdef HAVE_AVX512F
+			{asmlibAVX512FMemset, "asmlib AVX512F memset"},
+	#endif
+
+	#ifdef HAVE_AVX512BW
+			{asmlibAVX512BWMemset, "asmlib AVX512BW memset"},
+	#endif
+			{msvc2003Memset, "MSVC 2003 memset"},
+			{bytewiseMemset, "bytewise memset"},
+			{minixMemset, "minix memset"},
+			{freeBsdMemset, "FreeBSD memset"},
+			{inlineStringOpGccMemset, "-minline-all-stringops gcc memset"},
+			{inlineStringOpGccI386Memset, "-minline-all-stringops -march=i386 gcc memset"},
+			{inlineStringOpGccI486Memset, "-minline-all-stringops -march=i486 gcc memset"},
+			{inlineStringOpGccI686Memset, "-minline-all-stringops -march=i686 gcc memset"},
+			{inlineStringOpGccNoconaMemset, "-minline-all-stringops -march=nocona gcc memset"}
+		}
+	};
+
 	benchFunctions(funcs, std::cout);
 	return EXIT_SUCCESS;
 }
--- memset64.cpp
+++ memset64.cpp
@@ -18,27 +18,29 @@ extern "C"
 	mkMemsetDecl(inlineStringOpGccSkylakeMemset)
 }
 
-const static std::array<memsetFunc, 13> funcs =
-{
-	{
-		{memset, "system libc memset"},
-		{pdclibMemset, "pdclib memset"},
-		{cloudlibcMemset, "cloudlibc memset"},
-		{libFtMemset, "libft memset"},
-		{klibcMemset, "klibc memset"},
-		{neatlibcMemset, "neatlibc memset"},
-		{dietlibcMemset, "dietlibc memset"},
-		{uClibcMemset, "uClibc memset"},
-		{newlibMemset, "newlib memset"},
-		{muslMemset, "musl memset"},
-		{bionicSSE2SlmMemset, "bionic SSE2 memset"},
-		{inlineStringOpGccMemset, "-minline-all-stringops gcc memset"},
-		{inlineStringOpGccSkylakeMemset, "-minline-all-stringops -march=skylake gcc memset"}
-    }
-};
-
 int main()
 {
+	const static std::array<memsetFunc, 13> funcs =
+	{
+		{
+			{memset, "system libc memset"},
+			{pdclibMemset, "pdclib memset"},
+			{cloudlibcMemset, "cloudlibc memset"},
+			{libFtMemset, "libft memset"},
+			{klibcMemset, "klibc memset"},
+			{neatlibcMemset, "neatlibc memset"},
+			{dietlibcMemset, "dietlibc memset"},
+			{uClibcMemset, "uClibc memset"},
+			{newlibMemset, "newlib memset"},
+			{muslMemset, "musl memset"},
+	#ifdef HAVE_SSE2
+			{bionicSSE2SlmMemset, "bionic SSE2 memset"},
+	#endif
+			{inlineStringOpGccMemset, "-minline-all-stringops gcc memset"},
+			{inlineStringOpGccSkylakeMemset, "-minline-all-stringops -march=skylake gcc memset"}
+		}
+	};
+
 	benchFunctions(funcs, std::cout);
 	return EXIT_SUCCESS;
 }
